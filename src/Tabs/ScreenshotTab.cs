8f9bc6e08f5dbc963f8dd1ced79bfd4ea8a48076
alduris <152772325+alduris@users.noreply.github.com>
2024-03-10 00:12:09 -0500

    Finished right scrollbox

--- src/Tabs/ScreenshotTab.cs
+++ src/Tabs/ScreenshotTab.cs
@@ -27,6 +27,10 @@ namespace MapExporter.Tabs
             }
         }
         private const float SCROLLBAR_WIDTH = 20f;
+        private const float CHECKBOX_SIZE = 24f;
+        private const float LINE_HEIGHT = 20f;
+        private const float BIG_LINE_HEIGHT = 30f;
+
         private readonly Dictionary<string, HashSet<SlugcatStats.Name>> WaitingRegions = [];
         private readonly Queue<QueueData> QueuedRegions = [];
         private readonly Dictionary<string, string> RegionNames = [];
@@ -34,7 +38,6 @@ namespace MapExporter.Tabs
 
         private static readonly Color BlueColor = new(0.5f, 0.65f, 0.95f);
         private static readonly Color RedColor = new(0.85f, 0.5f, 0.55f);
-        private static readonly Color YellowColor = new(0.95f, 0.9f, 0.65f);
 
         private OpComboBox comboRegions;
         private OpScrollBox waitBox;
@@ -70,20 +73,22 @@ namespace MapExporter.Tabs
             comboRegions = new OpComboBox(OpUtil.CosmeticBind(""), new Vector2(10f, 530f), 175f, RegionNames.Keys.ToArray()) { listHeight = (ushort)Math.Min(20, RegionNames.Count) };
             var addButton = new OpSimpleButton(new Vector2(195f, 530f), new Vector2(40f, 24f), "ADD");
             var addAllButton = new OpSimpleButton(new Vector2(245f, 530f), new Vector2(40f, 24f), "ALL");
-            var startButton = new OpHoldButton(new Vector2(10f, 10f), new Vector2(80f, 24f), "START", 40f) { colorEdge = BlueColor };
-            var clearButton = new OpHoldButton(new Vector2(100f, 10f), new Vector2(80f, 24f), "CLEAR", 40f);
+            var startButton = new OpSimpleButton(new Vector2(10f, 10f), new Vector2(80f, 24f), "START") { colorEdge = BlueColor };
+            var clearButton = new OpSimpleButton(new Vector2(100f, 10f), new Vector2(80f, 24f), "CLEAR");
 
             waitBox = new OpScrollBox(new Vector2(10f, 50f), new Vector2(275f, 470f), 0f, false, true, true);
             queueBox = new OpScrollBox(new Vector2(315f, 10f), new Vector2(275f, 510f), 0f, false, true, true);
 
-            var abortButton = new OpHoldButton(new Vector2(315f, 530f), new Vector2(80f, 24f), "ABORT", 80f) { colorEdge = RedColor };
+            var abortButton = new OpSimpleButton(new Vector2(315f, 530f), new Vector2(80f, 24f), "ABORT") { colorEdge = RedColor };
             var skipButton = new OpSimpleButton(new Vector2(405f, 530f), new Vector2(80f, 24f), "SKIP");
 
             // Event listeners
             addButton.OnClick += AddButton_OnClick;
             addAllButton.OnClick += AddAllButton_OnClick;
-            startButton.OnPressDone += StartButton_OnPressDone;
-            clearButton.OnPressDone += ClearButton_OnPressDone;
+            startButton.OnClick += StartButton_OnClick;
+            clearButton.OnClick += ClearButton_OnClick;
+
+            abortButton.OnClick += AbortButton_OnClick;
 
             // Add UI to UI
             AddItems([
@@ -99,7 +104,7 @@ namespace MapExporter.Tabs
                 new OpImage(new(299f, 10f), "pixel") { scale = new Vector2(2f, 580f), color = MenuColorEffect.rgbMediumGrey },
 
                 // Right side
-                new OpLabel(315f, 560f, "QUEUE", true),
+                new OpLabel(315f, 560f, "SCREENSHOTTING", true),
                 abortButton,
                 skipButton,
                 queueBox,
@@ -111,6 +116,13 @@ namespace MapExporter.Tabs
 
         public override void Update()
         {
+            const float BIG_PAD = 12f; // big pad
+            const float SM_PAD = 6f;   // small pad
+            const float SEP_PAD = 18f; // separation pad
+            const float CHECKBOX_LH = CHECKBOX_SIZE + SM_PAD;
+            const float EDGE_PAD = SM_PAD;
+            const float QUEUE_SLUG_PAD = EDGE_PAD + SEP_PAD;
+
             // Left scrollbox update
             if (WaitDirty)
             {
@@ -127,50 +139,57 @@ namespace MapExporter.Tabs
                 // Calculate new size
                 if (WaitingRegions.Count > 0)
                 {
-                    var height = 24f;
-                    var y = waitBox.size.y + 4f; // 4f padding taken away from first element to make it 12f
+                    var height = BIG_PAD * 2f;
+                    var y = waitBox.size.y + (SEP_PAD - BIG_PAD);
 
                     foreach (var item in WaitingRegions)
                     {
                         // Delete button and text
-                        y -= 36f; // 20f line height, 16f top padding
-                        var delButton = new OpSimpleButton(new(6f, y), new(20f, 20f), "\xd7") { colorEdge = RedColor };
+                        y -= LINE_HEIGHT + SEP_PAD;
+                        var delButton = new OpSimpleButton(new(EDGE_PAD, y), new(LINE_HEIGHT, LINE_HEIGHT), "\xd7") { colorEdge = RedColor };
                         delButton.OnClick += _ =>
                         {
                             WaitingRegions.Remove(item.Key);
                             WaitDirty = true;
                         };
-                        var regionLabel = new OpLabel(32f, y, item.Key);
+                        var regionLabel = new OpLabel(EDGE_PAD + LINE_HEIGHT + SM_PAD, y, item.Key);
 
                         waitBox.AddItems(delButton, regionLabel);
 
                         // Buttons
-                        y -= 30f; // 24f button size, 6f top padding
+                        y -= CHECKBOX_LH;
                         var x = 0f;
                         int lines = 1;
                         for (int i = 0; i < Slugcats.Count; i++)
                         {
                             // Don't go out of the area
                             var textWidth = LabelTest.GetWidth(Slugcats[i].value);
-                            if (x + 36f > waitBox.size.x - SCROLLBAR_WIDTH - 12f)
+                            if (x + CHECKBOX_SIZE + BIG_PAD > waitBox.size.x - SCROLLBAR_WIDTH - 2 * EDGE_PAD)
                             {
                                 x = 0f;
-                                y -= 30f;
+                                y -= CHECKBOX_LH;
                                 lines++;
                             }
 
                             // Create checkbox
                             var j = i; // needed for variable reference purposese
                             var has = item.Value.Contains(Slugcats[i]);
-                            var checkbox = new OpCheckBox(OpUtil.CosmeticBind(has), new(6f + x, y))
+                            var checkbox = new OpCheckBox(OpUtil.CosmeticBind(has), new(EDGE_PAD + x, y))
                             {
                                 colorEdge = ScugDisplayColor(PlayerGraphics.DefaultSlugcatColor(Slugcats[i]), has),
                                 description = Slugcats[i].value
                             };
+
+                            // Create label
+                            x += CHECKBOX_SIZE + BIG_PAD;
+                            var scugLabel = new OpLabel(x, y, Slugcats[i].value, false) { color = checkbox.colorEdge };
+
+                            // Event listeners
                             checkbox.OnValueUpdate += (_, _, _) =>
                             {
                                 var scug = Slugcats[j];
-                                if (item.Value.Contains(scug))
+                                var c = item.Value.Contains(scug);
+                                if (c)
                                 {
                                     item.Value.Remove(scug);
                                 }
@@ -178,19 +197,17 @@ namespace MapExporter.Tabs
                                 {
                                     item.Value.Add(scug);
                                 }
-                                WaitDirty = true;
+                                checkbox.colorEdge = ScugDisplayColor(PlayerGraphics.DefaultSlugcatColor(Slugcats[j]), !c);
+                                scugLabel.color = checkbox.colorEdge;
+                                // WaitDirty = true;
                             };
-
-                            // Create label
-                            x += 36f;
-                            var scugLabel = new OpLabel(x, y, Slugcats[i].value, false) { color = checkbox.colorEdge };
 
                             // Add and adjust for next
                             waitBox.AddItems(checkbox, scugLabel);
-                            x += textWidth + 12f; // extra right padding
+                            x += textWidth + BIG_PAD; // extra right padding
                         }
 
-                        height += 36f + 30f * lines;
+                        height += LINE_HEIGHT + SEP_PAD + CHECKBOX_LH * lines;
                     }
 
                     waitBox.SetContentSize(height);
@@ -214,14 +231,57 @@ namespace MapExporter.Tabs
                 if (QueuedRegions.Count > 0)
                 {
                     var height = 24f;
-                    var y = queueBox.size.y;
+                    var y = queueBox.size.y + (BIG_PAD - EDGE_PAD);
+                    int i = 1;
 
                     foreach (var item in QueuedRegions)
                     {
-                        // Delete button and text
-                        y -= 32f; // 20f line height, 12f top padding
+                        y -= LINE_HEIGHT + BIG_PAD;
 
-                        // todo: the rest and add height
+                        // Current header
+                        if (i == 1)
+                        {
+                            y -= BIG_LINE_HEIGHT - LINE_HEIGHT;
+                            queueBox.AddItems(new OpLabel(EDGE_PAD, y, "Current:", true));
+                            y -= LINE_HEIGHT;
+                            height += BIG_LINE_HEIGHT;
+                        }
+                        else
+                        {
+                            height += BIG_PAD;
+                        }
+
+                        // Region name and scuglats
+                        queueBox.AddItems(new OpLabel(EDGE_PAD + SM_PAD, y, $"{i}. {item.name}", false));
+                        y -= LINE_HEIGHT;
+
+                        int lines = 2;
+                        float x = 0f;
+                        foreach (var scug in item.scugs)
+                        {
+                            var width = LabelTest.GetWidth(scug.value);
+                            if (x + width > queueBox.size.x - SCROLLBAR_WIDTH - QUEUE_SLUG_PAD - EDGE_PAD * 2)
+                            {
+                                x = 0f;
+                                y -= LINE_HEIGHT;
+                                lines++;
+                            }
+                            queueBox.AddItems(new OpLabel(x + QUEUE_SLUG_PAD, y, scug.value, false) { color = ScugEnabledColor(PlayerGraphics.DefaultSlugcatColor(scug)) });
+                            x += width + BIG_PAD;
+                        }
+                        height += LINE_HEIGHT * lines;
+
+                        // Horizontal line and queued header
+                        if (i == 1 && QueuedRegions.Count > 1)
+                        {
+                            y -= SM_PAD;
+                            queueBox.AddItems(new OpImage(new Vector2(EDGE_PAD, y), "pixel") { scale = new Vector2(queueBox.size.x - SCROLLBAR_WIDTH - EDGE_PAD * 2, 2f), color = MenuColorEffect.rgbMediumGrey });
+                            y -= 2f + SM_PAD + BIG_LINE_HEIGHT;
+                            height += 2 * SM_PAD + BIG_LINE_HEIGHT + 2f;
+                            queueBox.AddItems(new OpLabel(EDGE_PAD, y, "Queued:", true));
+                        }
+
+                        i++;
                     }
 
                     queueBox.SetContentSize(height);
@@ -271,7 +331,10 @@ namespace MapExporter.Tabs
                     WaitingRegions.Add(region.Key, scugs);
                     foreach (var scug in Slugcats)
                     {
-                        scugs.Add(scug);
+                        if (SlugcatStats.getSlugcatStoryRegions(scug).Contains(region.Value) || SlugcatStats.getSlugcatOptionalRegions(scug).Contains(region.Value))
+                        {
+                            scugs.Add(scug);
+                        }
                     }
                 }
             }
@@ -279,11 +342,11 @@ namespace MapExporter.Tabs
             WaitDirty = true;
         }
 
-        private void StartButton_OnPressDone(UIfocusable trigger)
+        private void StartButton_OnClick(UIfocusable trigger)
         {
             foreach (var region in WaitingRegions)
             {
-                if (!QueuedRegions.Any(x => x.Equals(region.Key)))
+                if (!QueuedRegions.Any(x => x.Equals(region.Key)) && region.Value.Count > 0)
                 {
                     QueuedRegions.Enqueue(new QueueData(region.Key, region.Value));
                 }
@@ -294,10 +357,16 @@ namespace MapExporter.Tabs
             QueueDirty = true;
         }
 
-        private void ClearButton_OnPressDone(UIfocusable trigger)
+        private void ClearButton_OnClick(UIfocusable trigger)
         {
             WaitingRegions.Clear();
             WaitDirty = true;
+        }
+
+        private void AbortButton_OnClick(UIfocusable trigger)
+        {
+            QueuedRegions.Clear();
+            QueueDirty = true;
         }
     }
 }
