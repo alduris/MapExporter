38baefbcb3711406fa78ce932c3a121f4fd7cdb7
alduris <152772325+alduris@users.noreply.github.com>
2024-04-04 20:53:49 -0400

    Simplified seed cob hook I think

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -83,7 +83,7 @@ sealed class Plugin : BaseUnityPlugin
                 IL.BubbleGrass.Update += BubbleGrass_Update;
                 On.MoreSlugcats.BlinkingFlower.DrawSprites += BlinkingFlower_DrawSprites;
                 On.RoomCamera.ApplyEffectColorsToPaletteTexture += RoomCamera_ApplyEffectColorsToPaletteTexture;
-                On.SeedCob.DrawSprites += SeedCob_DrawSprites;
+                On.SeedCob.FreezingPaletteUpdate += SeedCob_FreezingPaletteUpdate;
 
                 Logger.LogDebug("Finished start thingy");
             }
@@ -130,132 +130,12 @@ sealed class Plugin : BaseUnityPlugin
 
     #region fixes
 
-    private void SeedCob_DrawSprites(On.SeedCob.orig_DrawSprites orig, SeedCob self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+    // Fix seed cob crash for saint ig (sorry bensone I replaced your code copied straight from RW code :leditoroverload:)
+    private void SeedCob_FreezingPaletteUpdate(On.SeedCob.orig_FreezingPaletteUpdate orig, SeedCob self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
     {
-        Vector2 vector = Vector2.Lerp(self.firstChunk.lastPos, self.firstChunk.pos, timeStacker);
-        Vector2 vector2 = Vector2.Lerp(self.bodyChunks[1].lastPos, self.bodyChunks[1].pos, timeStacker);
-        float num = 0.5f;
-        Vector2 vector3 = self.rootPos;
-        for (int i = 0; i < self.stalkSegments; i++)
+        if (self.room != null)
         {
-            float f = (float)i / (self.stalkSegments - 1);
-            Vector2 vector4 = Custom.Bezier(self.rootPos, self.rootPos + self.rootDir * Vector2.Distance(self.rootPos, self.placedPos) * 0.2f, vector2, vector2 + Custom.DirVec(vector, vector2) * Vector2.Distance(rootPos, placedPos) * 0.2f, f);
-            Vector2 normalized = (vector3 - vector4).normalized;
-            Vector2 vector5 = Custom.PerpendicularVector(normalized);
-            float num2 = Vector2.Distance(vector3, vector4) / 5f;
-            float num3 = Mathf.Lerp(self.bodyChunkConnections[0].distance / 14f, 1.5f, Mathf.Pow(Mathf.Sin(Mathf.Pow(f, 2f) * (float)Math.PI), 0.5f));
-            float num4 = 1f;
-            Vector2 vector6 = default(Vector2);
-            for (int j = 0; j < 2; j++)
-            {
-                (sLeaser.sprites[self.StalkSprite(j)] as TriangleMesh).MoveVertice(i * 4, vector3 - normalized * num2 - vector5 * (num3 + num) * 0.5f * num4 - camPos + vector6);
-                (sLeaser.sprites[self.StalkSprite(j)] as TriangleMesh).MoveVertice(i * 4 + 1, vector3 - normalized * num2 + vector5 * (num3 + num) * 0.5f * num4 - camPos + vector6);
-                (sLeaser.sprites[self.StalkSprite(j)] as TriangleMesh).MoveVertice(i * 4 + 2, vector4 + normalized * num2 - vector5 * num3 * num4 - camPos + vector6);
-                (sLeaser.sprites[self.StalkSprite(j)] as TriangleMesh).MoveVertice(i * 4 + 3, vector4 + normalized * num2 + vector5 * num3 * num4 - camPos + vector6);
-                num4 = 0.35f;
-                vector6 += -rCam.room.lightAngle.normalized * num3 * 0.5f;
-            }
-
-            vector3 = vector4;
-            num = num3;
-        }
-
-        vector3 = vector2 + Custom.DirVec(vector, vector2);
-        num = 2f;
-        for (int k = 0; k < self.cobSegments; k++)
-        {
-            float t = (float)k / (self.cobSegments - 1);
-            Vector2 vector7 = Vector2.Lerp(vector2, vector, t);
-            Vector2 normalized2 = (vector3 - vector7).normalized;
-            Vector2 vector8 = Custom.PerpendicularVector(normalized2);
-            float num5 = Vector2.Distance(vector3, vector7) / 5f;
-            float num6 = 2f;
-            (sLeaser.sprites[self.CobSprite] as TriangleMesh).MoveVertice(k * 4, vector3 - normalized2 * num5 - vector8 * (num6 + num) * 0.5f - camPos);
-            (sLeaser.sprites[self.CobSprite] as TriangleMesh).MoveVertice(k * 4 + 1, vector3 - normalized2 * num5 + vector8 * (num6 + num) * 0.5f - camPos);
-            (sLeaser.sprites[self.CobSprite] as TriangleMesh).MoveVertice(k * 4 + 2, vector7 + normalized2 * num5 - vector8 * num6 - camPos);
-            (sLeaser.sprites[self.CobSprite] as TriangleMesh).MoveVertice(k * 4 + 3, vector7 + normalized2 * num5 + vector8 * num6 - camPos);
-            vector3 = vector7;
-            num = num6;
-        }
-
-        float num7 = Mathf.Lerp(self.lastOpen, self.open, timeStacker);
-        for (int l = 0; l < 2; l++)
-        {
-            float num8 = -1f + l * 2f;
-            num = 2f;
-            vector3 = vector + Custom.DirVec(vector2, vector) * 7f;
-            float num9 = Custom.AimFromOneVectorToAnother(vector, vector2);
-            Vector2 vector9 = vector;
-            for (int m = 0; m < self.cobSegments; m++)
-            {
-                float num10 = (float)m / (self.cobSegments - 1);
-                vector9 += Custom.DegToVec(num9 + num8 * Mathf.Pow(num7, Mathf.Lerp(1f, 0.1f, num10)) * 50f * Mathf.Pow(num10, 0.5f)) * (Vector2.Distance(vector, vector2) * 1.1f + 8f) / self.cobSegments;
-                Vector2 normalized3 = (vector3 - vector9).normalized;
-                Vector2 vector10 = Custom.PerpendicularVector(normalized3);
-                float num11 = Vector2.Distance(vector3, vector9) / 5f;
-                float num12 = Mathf.Lerp(2f, 6f, Mathf.Pow(Mathf.Sin(Mathf.Pow(num10, 0.5f) * (float)Math.PI), 0.5f));
-                (sLeaser.sprites[self.ShellSprite(l)] as TriangleMesh).MoveVertice(m * 4, vector3 - normalized3 * num11 - vector10 * (num12 + num) * 0.5f * (1 - l) - camPos);
-                (sLeaser.sprites[self.ShellSprite(l)] as TriangleMesh).MoveVertice(m * 4 + 1, vector3 - normalized3 * num11 + vector10 * (num12 + num) * 0.5f * l - camPos);
-                (sLeaser.sprites[self.ShellSprite(l)] as TriangleMesh).MoveVertice(m * 4 + 2, vector9 + normalized3 * num11 - vector10 * num12 * (1 - l) - camPos);
-                (sLeaser.sprites[self.ShellSprite(l)] as TriangleMesh).MoveVertice(m * 4 + 3, vector9 + normalized3 * num11 + vector10 * num12 * l - camPos);
-                vector3 = new Vector2(vector9.x, vector9.y);
-                num = num12;
-                num9 = Custom.VecToDeg(-normalized3);
-            }
-        }
-
-        if (num7 > 0f)
-        {
-            Vector2 vector11 = Custom.DirVec(vector2, vector);
-            Vector2 vector12 = Custom.PerpendicularVector(vector11);
-            for (int n = 0; n < self.seedPositions.Length; n++)
-            {
-                Vector2 vector13 = vector2 + vector11 * self.seedPositions[n].y * (Vector2.Distance(vector2, vector) - 10f) + vector12 * self.seedPositions[n].x * 3f;
-                float num13 = 1f + Mathf.Sin((float)n / (self.seedPositions.Length - 1) * (float)Math.PI);
-                if (self.AbstractCob.dead)
-                {
-                    num13 *= 0.5f;
-                }
-
-                sLeaser.sprites[self.SeedSprite(n, 0)].isVisible = true;
-                sLeaser.sprites[self.SeedSprite(n, 1)].isVisible = self.seedsPopped[n];
-                sLeaser.sprites[self.SeedSprite(n, 2)].isVisible = true;
-                sLeaser.sprites[self.SeedSprite(n, 0)].scale = (self.seedsPopped[n] ? num13 : 0.35f);
-                sLeaser.sprites[self.SeedSprite(n, 0)].x = vector13.x - camPos.x;
-                sLeaser.sprites[self.SeedSprite(n, 0)].y = vector13.y - camPos.y;
-                Vector2 vector14 = default;
-                if (self.seedsPopped[n])
-                {
-                    vector14 = vector12 * Mathf.Pow(Mathf.Abs(self.seedPositions[n].x), Custom.LerpMap(num13, 1f, 2f, 1f, 0.5f)) * Mathf.Sign(self.seedPositions[n].x) * 3.5f * num13;
-                    if (!self.AbstractCob.dead)
-                    {
-                        sLeaser.sprites[self.SeedSprite(n, 2)].element = Futile.atlasManager.GetElementWithName("tinyStar");
-                    }
-
-                    sLeaser.sprites[self.SeedSprite(n, 2)].rotation = Custom.VecToDeg(vector11);
-                    sLeaser.sprites[self.SeedSprite(n, 2)].scaleX = Mathf.Pow(1f - Mathf.Abs(self.seedPositions[n].x), 0.2f);
-                }
-
-                sLeaser.sprites[self.SeedSprite(n, 1)].x = vector13.x + vector14.x * 0.35f - camPos.x;
-                sLeaser.sprites[self.SeedSprite(n, 1)].y = vector13.y + vector14.y * 0.35f - camPos.y;
-                sLeaser.sprites[self.SeedSprite(n, 1)].scale = (self.seedsPopped[n] ? num13 : 0.4f) * 0.5f;
-                sLeaser.sprites[self.SeedSprite(n, 2)].x = vector13.x + vector14.x - camPos.x;
-                sLeaser.sprites[self.SeedSprite(n, 2)].y = vector13.y + vector14.y - camPos.y;
-            }
-        }
-
-        for (int num14 = 0; num14 < self.leaves.GetLength(0); num14++)
-        {
-            Vector2 vector15 = Vector2.Lerp(self.leaves[num14, 1], self.leaves[num14, 0], timeStacker);
-            sLeaser.sprites[self.LeafSprite(num14)].x = vector2.x - camPos.x;
-            sLeaser.sprites[self.LeafSprite(num14)].y = vector2.y - camPos.y;
-            sLeaser.sprites[self.LeafSprite(num14)].rotation = Custom.AimFromOneVectorToAnother(vector2, vector15);
-            sLeaser.sprites[self.LeafSprite(num14)].scaleY = Vector2.Distance(vector2, vector15) / 26f;
-        }
-
-        if (self.slatedForDeletetion || self.room != rCam.room)
-        {
-            sLeaser.CleanSpritesAndRemove();
+            orig(self, sLeaser, rCam);
         }
     }
 
