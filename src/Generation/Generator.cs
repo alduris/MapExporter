ca23f826d8d0a3bfd84fdf914f53badfa19c9756
alduris <152772325+alduris@users.noreply.github.com>
2024-06-08 17:27:17 -0400

    Started geometry parser

--- src/Generation/Generator.cs
+++ src/Generation/Generator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using RWCustom;
 using UnityEngine;
@@ -163,7 +164,14 @@ namespace MapExporter.Generation
                     }
                 case MetadataStep.Geometry:
                     {
-                        //
+                        List<GeometryInfo> geo = [];
+                        foreach (var room in regionInfo.rooms.Values)
+                        {
+                            if (room.size == default || room.size.x == 0 || room.size.y == 0) continue;
+                            geo.Add(ProcessRoomGeometry(room));
+                        }
+
+                        metadata["geometry_features"] = geo;
 
                         metadataStep = MetadataStep.Spawns;
                         break;
@@ -387,6 +395,105 @@ namespace MapExporter.Generation
             }
         }
 
+        private GeometryInfo ProcessRoomGeometry(RegionInfo.RoomEntry room)
+        {
+            List<(Vector2 A, Vector2 B)> lines = [];
+
+            // Create the lines
+            for (int j = 0; j < room.size.y; j++)
+            {
+                float y = room.devPos.y + j * 20f;
+                for (int i = 0; i < room.size.x; i++)
+                {
+                    float x = room.devPos.x + i * 20f;
+                    int[] tile = room.tiles[i, j];
+                    int type = tile[0];
+                    bool hpole = ((tile[1] & 1) == 1), vpole = ((tile[1] & 2) == 1);
+
+                    // Tile type
+                    switch (type)
+                    {
+                        case 0 or 1: // air and solid, respectively
+                            if (i != room.size.x - 1)
+                            {
+                                int neighbor = room.tiles[i + 1, j][0];
+                                if (type != neighbor && (neighbor & -2) == 0) // number & -2 will return 0 for either 0 or 1
+                                {
+                                    lines.Add((new(x, y), new(x, y + 20f)));
+                                }
+                            }
+                            if (j != room.size.y - 1)
+                            {
+                                int neighbor = room.tiles[i, j + 1][0];
+                                if (type != neighbor && (neighbor & -2) == 0)
+                                {
+                                    lines.Add((new(x, y), new(x + 20f, y)));
+                                }
+                            }
+                            break;
+                        case 2: // slopes
+                            // Need to check all four orientations, but there are only two cases of lines to draw.
+                            // In this, we are considering any check outside of the bounds of the room geometry to be solid.
+                            bool up = j == room.size.y - 1 || room.tiles[i, j + 1][0] == 1;
+                            bool down = j == 0 || room.tiles[i, j - 1][0] == 1;
+                            bool right = i == room.size.y - 1 || room.tiles[i + 1, j][0] == 1;
+                            bool left = i == 0 || room.tiles[i - 1, j][0] == 1;
+
+                            if (up == left && down == right)
+                            {
+                                lines.Add((new(x, y), new(x + 20f, y + 20f)));
+                            }
+                            else if (up == right && down == left)
+                            {
+                                lines.Add((new(x, y + 20f), new(x, y + 20f)));
+                            }
+                            break;
+                        case 3: // half floors
+                            // Top and bottom lines always get drawn
+                            lines.Add((new(x, y + 20f), new(x + 20f, y + 20f)));
+                            lines.Add((new(x, y + 10f), new(x + 20f, y + 10f)));
+
+                            // The edges get a little funky.
+                            // We don't draw if there is another half floor there but otherwise a line needs to be drawn *somewhere*.
+                            int l = i == 0 ? 3 : room.tiles[i - 1, j][0];
+                            int r = i == room.size.x - 1 ? 3 : room.tiles[i + 1, j][0];
+                            if (l != 3)
+                            {
+                                float o = l == 1 ? 0f : 10f;
+                                lines.Add((new(x, y + o), new(x, y + o + 10f)));
+                            }
+                            if (r != 3)
+                            {
+                                float o = l == 1 ? 0f : 10f;
+                                lines.Add((new(x + 20f, y + o), new(x + 20f, y + o + 10f)));
+                            }
+                            break;
+                        default: // anything else
+                            break;
+                    }
+
+                    // Poles
+                    if (type != 1) // don't draw poles if solid
+                    {
+                        if (hpole)
+                            lines.Add((new(x, y + 10f), new(x + 20f, y + 10f)));
+                        if (vpole)
+                            lines.Add((new(x + 10f, y), new(x + 10f, y + 20f)));
+                    }
+                }
+            }
+
+            // Optimize the lines (combining)
+            List<List<Vector2>> optimized = [];
+
+            // Return
+            return new GeometryInfo
+            {
+                room = room.roomName,
+                lines = optimized.Select(x => x.Select(Vec2arr).ToArray()).ToArray(),
+            };
+        }
+
         ///////////////////////////////////////////////////////////////////////////////////////////
         ///////////////////////////////////////////////////////////////////////////////////////////
 
@@ -446,6 +553,29 @@ namespace MapExporter.Generation
                         }
                     },
                     { "properties", new Dictionary<string, object> {} }
+                };
+            }
+        }
+
+        private struct GeometryInfo : IJsonObject
+        {
+            public string room;
+            public float[][][] lines;
+
+            public Dictionary<string, object> ToJson()
+            {
+                return new Dictionary<string, object>()
+                {
+                    { "type", "Feature" },
+                    {
+                        "geometry",
+                        new Dictionary<string, object>
+                        {
+                            { "type", "MultiLineString" },
+                            { "coordinates", lines }
+                        }
+                    },
+                    { "properties", new Dictionary<string, object> { { "room", room } } }
                 };
             }
         }
