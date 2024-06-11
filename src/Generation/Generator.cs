72afa9ea74afad75e7cb027c6c823b75b21d3ec3
alduris <152772325+alduris@users.noreply.github.com>
2024-06-11 16:04:02 -0400

    Switch to use IEnumerator instead of threads

--- src/Generation/Generator.cs
+++ src/Generation/Generator.cs
@@ -9,7 +9,7 @@ using static MapExporter.Generation.GenUtil;
 using Object = UnityEngine.Object;
 using RoomEntry = MapExporter.RegionInfo.RoomEntry;
 using DenSpawnData = MapExporter.RegionInfo.RoomEntry.DenSpawnData;
-using System.Threading;
+using IEnumerator = System.Collections.IEnumerator;
 
 namespace MapExporter.Generation
 {
@@ -20,7 +20,7 @@ namespace MapExporter.Generation
         private readonly RegionInfo regionInfo;
         private readonly bool skipExistingTiles;
 
-        private Task[] threads;
+        private IEnumerator[] tasks;
 
         public bool Done { get; private set; } = false;
         public bool Failed { get; private set; } = false;
@@ -68,44 +68,41 @@ namespace MapExporter.Generation
             {
                 case MetadataStep.Tiles:
                     {
-                        if (threads == null)
+                        if (tasks == null)
                         {
-                            threads = new Task[8];
-                            for (int i = 0; i < threads.Length; i++)
+                            tasks = new IEnumerator[8];
+                            for (int i = 0; i < tasks.Length; i++)
                             {
-                                int j = i;
-                                threads[i] = new Task(() => ProcessZoomLevel(-j));
-                                threads[i].Start();
+                                tasks[i] = ProcessZoomLevel(-i);
                             }
                         }
 
                         int tilesDone = 0;
                         int totalTiles = MetadataStepCount;
                         int tasksDone = 0;
-                        for (int i = 0; i < threads.Length; i++)
+                        for (int i = 0; i < tasks.Length; i++)
                         {
                             tilesDone += progress[i, 0];
                             totalTiles += progress[i, 1];
-
-                            var thread = threads[i];
-                            if (thread.IsCompleted)
+                        }
+                        for (int i = 0; i < tasks.Length; i++)
+                        {
+                            if (tasks[i].MoveNext())
+                            {
+                                break;
+                            }
+                            else
                             {
                                 tasksDone++;
-                            }
-                            else if (thread.IsFaulted)
-                            {
-                                Done = true;
-                                Failed = true;
-                                break;
                             }
                         }
 
                         Progress = (float)tilesDone / totalTiles;
 
-                        if (tasksDone == threads.Length)
+                        if (tasksDone == tasks.Length)
                         {
                             metadataStep = MetadataStep.Rooms;
-                            threads = null;
+                            tasks = null;
                         }
                         break;
                     }
@@ -251,10 +248,10 @@ namespace MapExporter.Generation
                 default: break;
             }
 
-            if (threads != null && metadataStep != MetadataStep.Tiles)
+            if (tasks != null && metadataStep != MetadataStep.Tiles)
             {
                 int count = 0;
-                for (int i = 0; i < threads.Length; i++)
+                for (int i = 0; i < tasks.Length; i++)
                 {
                     count += progress[i, 1];
                 }
@@ -270,151 +267,114 @@ namespace MapExporter.Generation
         private static readonly IntVector2 screenSizeInt = new(1400, 800);
         private static readonly Vector2    screenSize = screenSizeInt.ToVector2();
 
-        private void ProcessZoomLevel(int zoom)
+        private System.Collections.IEnumerator ProcessZoomLevel(int zoom)
         {
-            try
+            string outputPath = Directory.CreateDirectory(OutputPathForStep(zoom)).FullName;
+            float multFac = Mathf.Pow(2, zoom);
+
+            // Find room boundaries
+            Vector2 mapMin = Vector2.zero;
+            Vector2 mapMax = Vector2.zero;
+            foreach (var room in regionInfo.rooms.Values)
             {
-                string outputPath = Directory.CreateDirectory(OutputPathForStep(zoom)).FullName;
-                float multFac = Mathf.Pow(2, zoom);
-
-                // Find room boundaries
-                Vector2 mapMin = Vector2.zero;
-                Vector2 mapMax = Vector2.zero;
-                foreach (var room in regionInfo.rooms.Values)
+                if ((room.cameras?.Length ?? 0) == 0)
                 {
-                    if ((room.cameras?.Length ?? 0) == 0)
-                    {
-                        mapMin = new(Mathf.Min(room.devPos.x, mapMin.x), Mathf.Min(room.devPos.y, mapMin.y));
-                        mapMax = new(Mathf.Max(room.devPos.x + offscreenSize.x, mapMax.x), Mathf.Max(room.devPos.y + offscreenSize.y, mapMax.y));
-                    }
-                }
-
-                // Find tile boundaries (lower left inclusive, upper right non-inclusive)
-                IntVector2 llbTile = Vec2IntVecFloor(multFac * mapMin / tileSize);
-                IntVector2 urbTile = Vec2IntVecCeil(multFac * mapMax / tileSize);
-
-                // Make images
-                progress[-zoom, 0] = 0;
-                progress[-zoom, 1] = (urbTile.x - llbTile.x) * (urbTile.y - llbTile.y);
-                for (int tileY = llbTile.y; tileY <= urbTile.y; tileY++)
-                {
-                    // Attempt to save file reads. Camera textures are saved until an iteration is found where they aren't used.
-                    Dictionary<string, Texture2D> cameraCache = [];
-
-                    for (int tileX = llbTile.x; tileX <= urbTile.x; tileX++)
-                    {
-                        HashSet<string> encountered = []; // This is how we keep track of camera textures to clear. If they aren't in here, they were unused.
-                        Texture2D tile = null;
-
-                        // Get file path and see if we can skip it
-                        string filePath = Path.Combine(outputPath, $"{tileX}_{-1-tileY}.png");
-                        if (skipExistingTiles && File.Exists(filePath))
-                        {
-                            continue;
-                        }
-
-                        // Build tile
-                        var camPoint = new Vector2(tileX, tileY) * multFac;
-                        var camRect = new Rect(camPoint, tileSize);
-                        var tileCoords = new Vector2(tileX, tileY) * tileSize;
-
-                        foreach (var room in regionInfo.rooms.Values)
-                        {
-                            // Skip rooms with no cameras
-                            if (room.cameras == null || room.cameras.Length == 0) continue;
-
-                            for (int camNo = 0; camNo < room.cameras.Length; camNo++)
-                            {
-                                var cam = room.cameras[camNo];
-                                // Determine if the camera can be seen
-                                if (camRect.CheckIntersect(new Rect(cam * multFac, screenSize * multFac)))
-                                {
-                                    string fileName = $"{room.roomName}_{camNo}.png";
-
-                                    // Create the tile if necessary
-                                    if (tile == null)
-                                    {
-                                        tile = new Texture2D(tileSizeInt.x, tileSizeInt.y, TextureFormat.RGBA32, false, false);
-
-                                        // Fill with transparent color
-                                        var pixels = tile.GetPixels();
-                                        for (int i = 0; i < pixels.Length; i++)
-                                        {
-                                            pixels[i] = new Color(0f, 0f, 0f, 0f); // original implementation used fgcolor
-                                        }
-                                        tile.SetPixels(pixels);
-                                        tile.Apply();
-                                    }
-
-                                    // Open the camera so we can use it
-                                    Texture2D camTexture;
-                                    if (cameraCache.ContainsKey(fileName))
-                                    {
-                                        camTexture = cameraCache[fileName];
-                                    }
-                                    else
-                                    {
-                                        camTexture = new(screenSizeInt.x, screenSizeInt.y, TextureFormat.RGBA32, false, false);
-                                        camTexture.LoadImage(File.ReadAllBytes(Path.Combine(inputDir, fileName)));
-
-                                        if (zoom != 0) // No need to scale to the same resolution
-                                            ScaleTexture(camTexture, (int)(screenSizeInt.x * multFac), (int)(screenSizeInt.y * multFac));
-
-                                        camTexture.Apply();
-                                        cameraCache.Add(fileName, camTexture);
-                                    }
-                                    encountered.Add(fileName);
-
-                                    // Copy pixels
-                                    Vector2 copyOffsetVec = cam + Vector2.up * screenSize.y * multFac - tileCoords - Vector2.up * tileSize.y;
-                                    copyOffsetVec.x *= -1; // this makes it the flipped version of pasteoffset from the original script, which we need for the copy offset
-                                    IntVector2 copyOffset = Vec2IntVecFloor(copyOffsetVec);
-
-                                    int x = Math.Max(0, Math.Min(screenSizeInt.x, copyOffset.x));
-                                    int y = Math.Max(0, Math.Min(screenSizeInt.y, copyOffset.y));
-                                    int w = Math.Min(tileSizeInt.x - Math.Min(0, copyOffset.x), screenSizeInt.x - copyOffset.x);
-                                    int h = Math.Min(tileSizeInt.y - Math.Min(0, copyOffset.y), screenSizeInt.y - copyOffset.y);
-                                    var pixelData = camTexture.GetPixels(x, y, w, h);
-                                    tile.SetPixels(copyOffset.x < 0 ? -copyOffset.x : 0, copyOffset.y < 0 ? -copyOffset.y : 0, w, h, pixelData);
-                                }
-                            }
-                        }
-
-                        // Write tile if we drew anything
-                        if (tile != null)
-                        {
-                            tile.Apply();
-                            File.WriteAllBytes(Path.Combine(outputPath, $"{tileX}_{-1 - tileY}.png"), tile.EncodeToPNG());
-                            Object.Destroy(tile);
-                        }
-
-                        // Explodificate unused camera textures to save memory
-                        var keys = cameraCache.Keys;
-                        foreach (var key in keys)
-                        {
-                            if (!encountered.Contains(key))
-                            {
-                                Object.Destroy(cameraCache[key]);
-                                cameraCache.Remove(key);
-                            }
-                        }
-
-                        // Update progress
-                        progress[-zoom, 0]++;
-                    }
-
-                    // The cameras likely won't be used when we wrap around to the other side again so just destroy them all.
-                    // Also this is the end of scope for cameraCache anyway lol
-                    foreach (var cam in cameraCache)
-                    {
-                        Object.Destroy(cam.Value);
-                    }
+                    mapMin = new(Mathf.Min(room.devPos.x, mapMin.x), Mathf.Min(room.devPos.y, mapMin.y));
+                    mapMax = new(Mathf.Max(room.devPos.x + offscreenSize.x, mapMax.x), Mathf.Max(room.devPos.y + offscreenSize.y, mapMax.y));
                 }
             }
-            catch (Exception e)
+
+            // Find tile boundaries (lower left inclusive, upper right non-inclusive)
+            IntVector2 llbTile = Vec2IntVecFloor(multFac * mapMin / tileSize);
+            IntVector2 urbTile = Vec2IntVecCeil(multFac * mapMax / tileSize);
+
+            // Make images
+            progress[-zoom, 0] = 0;
+            progress[-zoom, 1] = (urbTile.x - llbTile.x) * (urbTile.y - llbTile.y);
+            for (int tileY = llbTile.y; tileY <= urbTile.y; tileY++)
             {
-                Plugin.Logger.LogError("Zoom level " + zoom + " failed with the following error: " + e);
-                throw;
+                for (int tileX = llbTile.x; tileX <= urbTile.x; tileX++)
+                {
+                    Texture2D tile = null;
+
+                    // Get file path and see if we can skip it
+                    string filePath = Path.Combine(outputPath, $"{tileX}_{-1-tileY}.png");
+                    if (skipExistingTiles && File.Exists(filePath))
+                    {
+                        continue;
+                    }
+
+                    // Build tile
+                    var camPoint = new Vector2(tileX, tileY) * multFac;
+                    var camRect = new Rect(camPoint, tileSize);
+                    var tileCoords = new Vector2(tileX, tileY) * tileSize;
+
+                    foreach (var room in regionInfo.rooms.Values)
+                    {
+                        // Skip rooms with no cameras
+                        if (room.cameras == null || room.cameras.Length == 0) continue;
+
+                        for (int camNo = 0; camNo < room.cameras.Length; camNo++)
+                        {
+                            var cam = room.cameras[camNo];
+                            // Determine if the camera can be seen
+                            if (camRect.CheckIntersect(new Rect(cam * multFac, screenSize * multFac)))
+                            {
+                                string fileName = $"{room.roomName}_{camNo}.png";
+
+                                // Create the tile if necessary
+                                if (tile == null)
+                                {
+                                    tile = new Texture2D(tileSizeInt.x, tileSizeInt.y, TextureFormat.RGBA32, false, false);
+
+                                    // Fill with transparent color
+                                    var pixels = tile.GetPixels();
+                                    for (int i = 0; i < pixels.Length; i++)
+                                    {
+                                        pixels[i] = new Color(0f, 0f, 0f, 0f); // original implementation used fgcolor
+                                    }
+                                    tile.SetPixels(pixels);
+                                    tile.Apply();
+                                }
+
+                                // Open the camera so we can use it
+                                Texture2D camTexture;
+                                camTexture = new(screenSizeInt.x, screenSizeInt.y, TextureFormat.RGBA32, false, false);
+                                camTexture.LoadImage(File.ReadAllBytes(Path.Combine(inputDir, fileName)), false);
+
+                                if (zoom != 0) // No need to scale to the same resolution
+                                    ScaleTexture(camTexture, (int)(screenSizeInt.x * multFac), (int)(screenSizeInt.y * multFac));
+
+                                camTexture.Apply();
+
+                                // Copy pixels
+                                Vector2 copyOffsetVec = cam + Vector2.up * screenSize.y * multFac - tileCoords - Vector2.up * tileSize.y;
+                                copyOffsetVec.x *= -1; // this makes it the flipped version of pasteoffset from the original script, which we need for the copy offset
+                                IntVector2 copyOffset = Vec2IntVecFloor(copyOffsetVec);
+
+                                int x = Math.Max(0, Math.Min(screenSizeInt.x, copyOffset.x));
+                                int y = Math.Max(0, Math.Min(screenSizeInt.y, copyOffset.y));
+                                int w = Math.Min(tileSizeInt.x - Math.Min(0, copyOffset.x), screenSizeInt.x - copyOffset.x);
+                                int h = Math.Min(tileSizeInt.y - Math.Min(0, copyOffset.y), screenSizeInt.y - copyOffset.y);
+                                var pixelData = camTexture.GetPixels(x, y, w, h);
+                                tile.SetPixels(copyOffset.x < 0 ? -copyOffset.x : 0, copyOffset.y < 0 ? -copyOffset.y : 0, w, h, pixelData);
+                                Object.Destroy(camTexture);
+                            }
+                        }
+                    }
+
+                    // Write tile if we drew anything
+                    if (tile != null)
+                    {
+                        tile.Apply();
+                        File.WriteAllBytes(Path.Combine(outputPath, $"{tileX}_{-1 - tileY}.png"), tile.EncodeToPNG());
+                        Object.Destroy(tile);
+                    }
+
+                    // Update progress
+                    progress[-zoom, 0]++;
+                    yield return null;
+                }
             }
         }
 
