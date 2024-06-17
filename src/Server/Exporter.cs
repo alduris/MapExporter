67dbac98a4e65397e1ba5e0afe823f8272ccfc76
alduris <152772325+alduris@users.noreply.github.com>
2024-06-17 03:28:14 -0400

    Basic directory explorer

---
+++ src/Server/Exporter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace MapExporter.Server
+{
+    internal static class Exporter
+    {
+        public static void ExportServer(ExportType exportType, string fileLocation)
+        {
+            if (exportType == ExportType.None) return;
+
+            // Get temporary directory
+            var tempDir = Path.GetTempPath();
+
+            // Copy frontend files
+            //
+
+            // Copy output files
+            //
+
+            // Copy self-host executable if wanted
+            if (exportType == ExportType.Server)
+            {
+                //
+            }
+
+            ZipFile.CreateFromDirectory(tempDir, Path.Combine(fileLocation, "mapexport.zip"));
+
+            // Delete our temporary dir
+            new DirectoryInfo(tempDir).Delete();
+        }
+
+        public enum ExportType
+        {
+            /// <summary>
+            /// Default value, does nothing
+            /// </summary>
+            None,
+            /// <summary>
+            /// Full batch with no self-host file
+            /// </summary>
+            NoServer,
+            /// <summary>
+            /// Full batch with self-host file
+            /// </summary>
+            Server
+        }
+    }
+}
--- src/Tabs/ServerTab.cs
+++ src/Tabs/ServerTab.cs
@@ -1,32 +1,27 @@
 ﻿using System.Collections.Generic;
 using MapExporter.Server;
+using MapExporter.Tabs.UI;
 using Menu;
 using Menu.Remix.MixedUI;
 using UnityEngine;
 
 namespace MapExporter.Tabs
 {
-    internal class ServerTab : BaseTab
+    internal class ServerTab(OptionInterface owner) : BaseTab(owner, "Run")
     {
         private static LocalServer server;
         private Queue<string> undisplayedMessages = [];
+        private OpScrollBox messageBox;
 
-        public ServerTab(OptionInterface owner) : base(owner, "Run")
-        {
-            OnPreDeactivate += ServerTab_OnPreDeactivate;
-        }
-
-        private void ServerTab_OnPreDeactivate()
-        {
-            if (server != null)
-            {
-                server.OnMessage -= Server_OnMessage;
-            }
-        }
+        private OpDirPicker dirPicker;
 
         public override void Initialize()
         {
-            var button = new OpSimpleButton(new Vector2(300f - 30f, 300f - 12f), new Vector2(60f, 24f), server == null ? "RUN" : "STOP");
+            const float PADDING = 10f;
+            const float MARGIN = 6f;
+            const float DIVIDER = MENU_SIZE * 0.5f;
+
+            var button = new OpSimpleButton(new Vector2(300f - 30f, 500f - 12f), new Vector2(60f, 24f), server == null ? "RUN" : "STOP");
             button.OnClick += (_) =>
             {
                 if (server != null)
@@ -43,10 +38,18 @@ namespace MapExporter.Tabs
                     button.text = "STOP";
                 }
             };
+            messageBox = new OpScrollBox(
+                new Vector2(PADDING, MENU_SIZE - PADDING - BIG_LINE_HEIGHT - MARGIN),
+                new Vector2(MENU_SIZE - 2 * PADDING, MENU_SIZE - DIVIDER - 2 * PADDING - MARGIN - BIG_LINE_HEIGHT - LINE_HEIGHT),
+                0f);
+            dirPicker = new OpDirPicker(new Vector2(PADDING, PADDING), new Vector2(MENU_SIZE - 2 * PADDING, DIVIDER - 2 * PADDING - MARGIN - BIG_LINE_HEIGHT - 24f));
             AddItems(
-                new OpLabel(10f, 560f, "SERVER", true),
+                new OpLabel(PADDING, MENU_SIZE - PADDING - BIG_LINE_HEIGHT, "SERVER", true),
                 button,
-                new OpImage(new Vector2(10f, 299f), "pixel") { scale = new Vector2(580f, 2f), color = MenuColorEffect.rgbMediumGrey }
+                messageBox,
+                new OpImage(new Vector2(PADDING, DIVIDER - 1), "pixel") { scale = new Vector2(MENU_SIZE - PADDING * 2, 2f), color = MenuColorEffect.rgbMediumGrey },
+                new OpLabel(PADDING, DIVIDER - PADDING - BIG_LINE_HEIGHT, "EXPORT", true),
+                dirPicker
             );
 
             Resources.CopyFrontendFiles();
